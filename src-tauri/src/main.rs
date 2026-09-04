a35846e0c3b901b8d17fcb378da581ad4340dcff
Shubham Singh <itzshubhamkashyap@gmail.com>
2026-09-04 12:04:24 +0530

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running EDITFLOW OS application");
+    editflow_os::run();
 }
